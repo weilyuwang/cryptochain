9619221cd2c61b0080dcd52ac9ec5517400547f8
weilyuwang <weilyuwang@gmail.com>
2021-04-25 20:17:21 -0400

    :camera_flash: hash function

---
+++ crypto-hash.test.js
@@ -0,0 +1,17 @@
+const cryptoHash = require("./crypto-hash");
+
+const TEXT = "foo";
+const HASHED_TEXT =
+  "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae";
+
+describe("cryptoHash()", () => {
+  it("generates a SHA-356 hashed output", () => {
+    expect(cryptoHash(TEXT)).toEqual(HASHED_TEXT);
+  });
+
+  it("produces the same hash with the same input arguments in any order", () => {
+    expect(cryptoHash("one", "two", "three")).toEqual(
+      cryptoHash("three", "one", "two")
+    );
+  });
+});
