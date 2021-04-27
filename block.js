6bc9983ba58b6de5339b2099c36c1c83b248be18
weilyuwang <weilyuwang@gmail.com>
2021-04-27 17:22:42 -0400

    :fire: refactor

--- block.js
+++ block.js
@@ -1,5 +1,6 @@
 const { GENESIS_DATA, MINE_RATE } = require("./config");
 const cryptoHash = require("./crypto-hash");
+const hexToBinary = require("hex-to-binary");
 
 class Block {
   constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
@@ -32,7 +33,9 @@ class Block {
         timestamp,
       });
       hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-    } while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
+    } while (
+      hexToBinary(hash).substring(0, difficulty) !== "0".repeat(difficulty)
+    );
 
     return new this({
       timestamp,
--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -7,7 +7,7 @@ const HASHED_TEXT =
 
 describe("cryptoHash()", () => {
   it("generates a SHA-356 hashed output", () => {
-    expect(cryptoHash(TEXT)).toEqual(hexToBinary(HASHED_TEXT));
+    expect(cryptoHash(TEXT)).toEqual(HASHED_TEXT);
   });
 
   it("produces the same hash with the same input arguments in any order", () => {
