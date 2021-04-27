1b3d0107355701f071d9baeb6b369d06c916e3ed
weilyuwang <weilyuwang@gmail.com>
2021-04-27 17:36:29 -0400

    :rewind: prevent difficulty jumps

--- blockchain.js
+++ blockchain.js
@@ -23,8 +23,9 @@ class Blockchain {
 
     for (let i = 1; i < chain.length; i++) {
       const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
-
       const actualLastHash = chain[i - 1].hash;
+      const lastDifficulty = chain[i - 1].difficulty;
+
       if (lastHash !== actualLastHash) return false;
 
       const validatedHash = cryptoHash(
@@ -35,6 +36,8 @@ class Blockchain {
         data
       );
       if (validatedHash !== hash) return false;
+
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false;
     }
 
     return true;
