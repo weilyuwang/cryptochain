b27e87654003bdd70c4de88c609326460be9ccdb
weilyuwang <weilyuwang@gmail.com>
2021-04-26 22:45:02 -0400

    :fire: implement validatedChain method

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
   constructor() {
@@ -12,6 +13,26 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    // compare string formats instead of two instances directly
+    // two instances created from same class are different (!==) in JavaScript
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+      return false;
+
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+
+      const actualLastHash = chain[i - 1].hash;
+      const { timestamp, lastHash, hash, data } = block;
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+      if (validatedHash !== hash) return false;
+    }
+
+    return true;
   }
 }
 
