4b60a6bbfba8a7338431a49160466b8aa6f64e19
weilyuwang <weilyuwang@gmail.com>
2021-04-27 15:48:51 -0400

    :sparkles: implement the mineblock function to pass the tests

--- block.js
+++ block.js
@@ -1,12 +1,14 @@
-const { GENESIS_DATA } = require("./config");
+const { GENESIS_DATA, INITIAL_DIFFICULTY } = require("./config");
 const cryptoHash = require("./crypto-hash");
 
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
 
   //  A Genesis Block is the name given to the first block a cryptocurrency
@@ -15,13 +17,26 @@ class Block {
   }
 
   static mineBlock({ lastBlock, data }) {
-    const timestamp = Date.now();
+    let hash, timestamp;
     const lastHash = lastBlock.hash;
+    const difficulty = lastBlock.difficulty;
+    // we want the nonce value to be dynamically adjustable
+    let nonce = 0;
+
+    do {
+      // update nonce and timestamp until we found a solution
+      nonce++;
+      timestamp = Date.now();
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
+
     return new this({
       timestamp,
       lastHash,
       data,
-      hash: cryptoHash(timestamp, lastHash, data),
+      difficulty,
+      nonce,
+      hash,
     });
   }
 }
--- config.js
+++ config.js
@@ -1,8 +1,12 @@
+const INITIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
   timestamp: 1,
   lastHash: "______",
   hash: "hash-one",
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
   data: [],
 };
 
-module.exports = { GENESIS_DATA };
+module.exports = { GENESIS_DATA, INITIAL_DIFFICULTY };
