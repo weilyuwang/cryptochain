86a0f5e7efe4b57cd0200e54d1400d13c1304677
weilyuwang <weilyuwang@gmail.com>
2021-05-26 23:32:04 -0400

    :triangular_flag_on_post: post request to mine block

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const Blockchain = require("./blockchain");
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(express.json()); // Used to parse JSON request bodies
+
 app.get("/api/blocks", (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+
+  res.redirect("/api/blocks");
 });
 
 const PORT = 5000;
